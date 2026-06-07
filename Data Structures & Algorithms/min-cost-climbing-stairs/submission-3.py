031b48276b6fd12742775a86ac7f4416f5b49602
RockkLee <50697724+RockkLee@users.noreply.github.com>
2026-06-07 21:34:40 +0800

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+
+        # @lru_cache
+        # def topdown(i: int) -> int:
+        #     if i >= n:
+        #         return 0
+        #     return min(cost[i] + topdown(i + 1), cost[i] + topdown(i + 2))
+        # return min(topdown(0), topdown(1))
+        if n == 1:
+            return cost[0]
+        if n == 2:
+            return min(cost[0], cost[1])
+        bottomup = [0] * n
+        bottomup[0] = cost[0]
+        bottomup[1] = cost[1]
+        for idx in range(2, n): 
+            bottomup[idx] = cost[idx] + min(bottomup[idx - 1], bottomup[idx - 2])
+        print(bottomup)
+        return min(bottomup[len(cost) - 1], bottomup[len(cost) - 2])
