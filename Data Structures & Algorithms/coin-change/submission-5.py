814bcea80ca135415a3befbde6d7a49b4527809c
RockkLee <50697724+RockkLee@users.noreply.github.com>
2026-06-26 15:44:48 +0800

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.py
@@ -0,0 +1,19 @@
+from functools import cache
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins = sorted(coins, reverse=True)
+        @cache
+        def topdown(amt: int):
+            if amt == 0:
+                return 0
+            res = 1e91
+            for coin in coins:
+                nxt_amt = amt - coin
+                if nxt_amt >= 0:
+                    res = min(res, 1 + topdown(nxt_amt))
+            return res
+        
+        minCoins = topdown(amount)
+        return -1 if minCoins >= 1e9 else int(minCoins)
